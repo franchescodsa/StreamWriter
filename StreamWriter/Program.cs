cd48c5bfc730f471a03a7ba887589fb9a94a6e33
franchesco aprigio <franchescodsa@outlook.com>
2024-07-02 20:12:29 -0300

    Adicionar arquivos de projeto.

---
+++ StreamWriter/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace StreamWriterr
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string sourcepath = @"f:\tempp\teste.txt";
+            string targetpath = @"f:\tempp\teste2.txt";
+
+            try
+            {
+                string[] lines = File.ReadAllLines(sourcepath);
+
+                using (StreamWriter sw = File.AppendText(targetpath))
+                {
+                    foreach (string line in lines)
+                    {
+                        sw.WriteLine(line.ToUpper());
+                    }
+
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("Erro inesperado");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
